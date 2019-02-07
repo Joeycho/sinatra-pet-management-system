b18df4c69c687da72603e531d02e7826fb8bb5d7
Joey Cho <cjy12182000@naver.com>
2019-02-07 09:57:45 +0000

    create pet process

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,23 @@
 class PetsController < ApplicationController
 
+  get '/pets/new' do
+    if session[:owner_id]==nil
+        redirect "/login"
+      else
+        erb :"/pets/new"
+      end
+  end
+
+  post '/pets' do
+      params[:pets].all.each do |pet|
+
+
+          if params[:pet_name]!=""
+            @pet = Pet.create(:name => params[:pet_name], :owner_id => session[:owner_id])
+            @pet.save
+          end
+
+          redirect "/owners/#{Owner.find(session[:owner_id])}"
+
+  end
 end
