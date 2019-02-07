1d0aaa83346410e9dd82dc1cab3a09be0fa314e0
Joey Cho <cjy12182000@naver.com>
2019-02-07 12:24:17 +0000

    Done with update and delete functions on pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,6 +23,44 @@ class PetsController < ApplicationController
           end
 
           redirect "/owners/#{session[:owner_id]}"
-
     end
+
+    get "/pets/:id/edit" do
+    if session[:owner_id]==nil && session[:owner_id] != Pet.find(params[:id]).owner.id
+        redirect "/login"
+    else
+      @pet = Pet.find(params[:id])
+      erb :"/pets/edit"
+    end
+  end
+
+    patch '/pets/:id' do
+        @pet = Pet.find_by_id(params[:id])
+      if params[:pet_name]!=""
+        @pet.name = params[:pet_name]
+        @pet.save
+        redirect to "/owners/#{session[:owner_id]}"
+      else
+        redirect to "/pets/#{@pet.id}/edit"
+      end
+    end
+
+    patch '/pets/:id/bye' do
+        @pet = Pet.find(params[:id])
+        @pet.owner_id = params[:owner][:name]
+        @pet.save
+        redirect to "/owners/#{session[:owner_id]}"
+
+        redirect to "/pets/#{@pet.id}/edit"
+    end
+
+    delete '/pets/:id' do
+    @pet = Pet.find_by_id(params[:id])
+    if session[:owner_id] == @pet.owner_id
+      @pet.delete
+    end
+      redirect to "/owners/#{session[:owner_id]}"
+  end
+
+
 end
